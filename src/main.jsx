59093f63ab43fd27eef08afc82f5f0714b5059ca
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-10-05 15:21:24 -0300

    configured Styled-Components

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import GlobalStyle from './styles/globalStyles';
+import { standardTheme } from './styles/themes/standard.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  
+    <ThemeProvider theme={standardTheme}>
+      <GlobalStyle />
+    </ThemeProvider>
   </StrictMode>,
-)
+);
