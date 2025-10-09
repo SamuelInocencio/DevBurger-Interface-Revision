846b15be40ccd13d6eaa5b9713714d6b7202648f
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-10-09 11:09:40 -0300

    configured RouterProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { ThemeProvider } from 'styled-components';
-import { Login } from './containers/Login/index.jsx';
+import { router } from './routes';
 import GlobalStyle from './styles/globalStyles';
 import { standardTheme } from './styles/themes/standard.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={standardTheme}>
-      <Login />
+      <RouterProvider router={router} />
       <GlobalStyle />
       <ToastContainer />
     </ThemeProvider>
