e2b2ba296addf7ad82dd9530efbd1cd55107695b
SamuelInocencio <samuelferreirainocencio@gmamil.com>
2025-10-09 12:31:15 -0300

    implemented registerScreen

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,2 +1,5 @@
 export * from './Login';
+export * from './Register';
+
+
 
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import { Login } from '../containers/Login';
+import { Login, Register } from '../containers/index';
 
 export const router = createBrowserRouter([
   {
+    path: '/',
+    element: <>{/* <Home /> */}</>,
+  },
+  {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/cadastro',
+    element: <Register />,
   },
 ]);
